ffbefee0e5cbcb1176e1a56007a082287373df33
LeonK <52380118+LeonK-UnnaturalD@users.noreply.github.com>
2019-11-24 20:45:48 +0100

    fix socket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
-const config: SocketIoConfig = { url: 'http://localhost:8080', options: {} };
+const config: SocketIoConfig = { url: 'https://findpinearyou.herokuapp.com', options: {} };
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
