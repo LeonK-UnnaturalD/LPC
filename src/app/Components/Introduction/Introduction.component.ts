1167124e62ef633ecc4eb907e5368dbc99ccb999
LeonK <52380118+LeonK-UnnaturalD@users.noreply.github.com>
2019-11-24 20:40:18 +0100

    fix

--- src/app/Components/Introduction/Introduction.component.ts
+++ src/app/Components/Introduction/Introduction.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Introduction.component.css']
 })
 export class IntroductionComponent implements OnInit {
+  public CustomerCount: number = 1000;
+  public CountryCount: number = 274;
 
   constructor() { }
 
